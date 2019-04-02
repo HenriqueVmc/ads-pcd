030533841858bad2ea69f9a49d789b633c1dfbc0
GR13V0U5 <adsroot@adsroot.com>
2019-04-01 22:01:01 -0300

    Iniciando trabalho  de PCD

---
+++ projSimuladorMercado/src/pct/Anotations.java
@@ -0,0 +1,53 @@
+package pct;
+
+// --- Mapeamento do projeto --- //
+
+// Itens de Compra -> Buffer(?)
+//				- get Itens
+//				- set Itens
+//
+
+// Carrinho -> Buffer (?)
+// 				- get Carrinho
+//				- set Carrinho
+//
+
+// Comprador -> Consumidor (Thread)
+//				- get Carrinho
+//				- get Itens
+//				- set Carrinho
+//
+
+// Esteira -> Buffer
+//				- set Itens
+//				- get Itens 
+//
+
+// Caixa -> Produtor (Thread)
+// 				- get Esteira 
+//				- set Empacotamento
+//
+
+// Rampa de Empacotametno -> Buffer
+//				- set Itens -> Caixa
+//				- get Itens -> Empacotador
+//
+
+// Empacotador -> Consumidor(Thread)
+//				- get Rampa
+//				- set Sacola
+//
+
+// --- CENÁRIO --- //
+// Comprador -> getCarrinho
+// Comprador -> getItens
+// Comprador -> setCarrinho(Itens)
+// Comprador/Carrinho -> setEsteira(Itens)
+// Caixa -> getEsteira
+// Caixa -> setRampa(Itens)
+// -- Valor Total --
+// Empacotador -> getRampa
+// Empacotador -> setSacola
+
+
+// Teremos Threads Produtoras, Consumidoras e Buffers :
---
+++ projSimuladorMercado/src/pct/Buffer.java
@@ -0,0 +1,7 @@
+package pct;
+
+public interface Buffer {
+
+   public void set( int value );
+   public int get();
+}
